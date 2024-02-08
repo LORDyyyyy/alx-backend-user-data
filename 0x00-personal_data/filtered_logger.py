c4d7634c8c53753a57d547e50978193851b5fc35
LORDyyyyy <mlord6271@gmail.com>
2024-02-08 15:06:13 +0200

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,7 +54,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Connect to secure database """
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'root')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
 
     mydb = mysql.connector.connect(

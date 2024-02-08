2b4442ab044f8b63017b2a59747d33833de55545
LORDyyyyy <mlord6271@gmail.com>
2024-02-08 13:54:49 +0200

    task 0 and gitignore

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Filtered Logger """
+import re
+from typing import (List)
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: List, separator: str) -> List[str]:
+    """ returns the log message obfuscated """
+    for f in fields:
+        message = re.sub(r'({})=.*?{}'.format(f, separator), r'\1={}{}'.format(redaction, separator), message)
+    return message

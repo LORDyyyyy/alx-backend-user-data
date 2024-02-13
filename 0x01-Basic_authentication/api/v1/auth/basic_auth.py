d4d5482a1314ff177860f5a4b5159f18f0f59fd7
LORDyyyyy <mlord6271@gmail.com>
2024-02-13 14:45:20 +0200

    task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,6 +54,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email: str, user_pwd: str
                                      ) -> TypeVar('User'):
         """  that returns the User instance based on his email and password """
+        if user_pwd is None or user_email is None:
+            return None
         if not isinstance(user_email, str) or not isinstance(user_pwd, str):
             return None
 
@@ -68,4 +70,27 @@ class BasicAuth(Auth):
             return None
         if not user.is_valid_password(user_pwd):
             return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = super().authorization_header(request)
+        if auth_header is None:
+            return None
+
+        auth_header_value = self.extract_base64_authorization_header(
+            auth_header)
+        if auth_header_value is None:
+            return None
+
+        auth_header_decoded_value = self.decode_base64_authorization_header(
+                                    auth_header_value)
+        if auth_header_decoded_value is None:
+            return None
+
+        user_data = self.extract_user_credentials(auth_header_decoded_value)
+        if user_data == (None, None):
+            return None
+
+        user = self.user_object_from_credentials(user_data[0], user_data[1])
         return user

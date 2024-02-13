f0a2b2fb0f031b4075f9a64b56ce56278c6e5e1f
LORDyyyyy <mlord6271@gmail.com>
2024-02-13 13:01:55 +0200

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,12 @@ class Auth():
     """ a class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Will be Implemented """
-        return False
+        """ Define which routes don't need authentication """
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Will be Implemented """

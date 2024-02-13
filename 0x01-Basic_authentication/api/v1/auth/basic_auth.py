924c604a112d71bcbad0c0d79726ce2d18655441
LORDyyyyy <mlord6271@gmail.com>
2024-02-13 13:38:45 +0200

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ The Basic Auth """
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.split()[-1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None or not isinstance(
+                       base64_authorization_header, str):
+            return None
+        try:
+            value = base64.b64decode(base64_authorization_header)
+            return value.decode('utf-8')
+        except:
+            return None

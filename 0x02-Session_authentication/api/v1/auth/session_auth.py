9ddd2bd09211ce621962d1585041a26b56a304ca
LORDyyyyy <mlord6271@gmail.com>
2024-02-15 20:30:51 +0200

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" SessionAuth Class """
+from api.v1.auth.auth import Auth
+# from typing import TypeVar
+# import base64
+# from models.user import User
+
+
+class SessionAuth(Auth):
+    """ The Session Authentication Class"""

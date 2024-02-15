5c562242f9b9b0b3b6fc3d356fc9930ff48d229b
LORDyyyyy <mlord6271@gmail.com>
2024-02-15 21:10:03 +0200

    task 4

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,10 +39,8 @@ def before_request():
                                             '/api/v1/auth_session/login/']):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None\
+            and auth.session_cookie(request) is None:
         abort(401)
 
     curr_user = auth.current_user(request)

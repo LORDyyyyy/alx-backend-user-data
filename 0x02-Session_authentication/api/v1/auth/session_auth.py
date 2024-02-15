cafe5059a5e182865e8a9d68f808f82c8b5d90db
LORDyyyyy <mlord6271@gmail.com>
2024-02-15 20:36:30 +0200

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ SessionAuth Class """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 # from typing import TypeVar
 # import base64
 # from models.user import User
@@ -8,3 +9,15 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """ The Session Authentication Class"""
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id

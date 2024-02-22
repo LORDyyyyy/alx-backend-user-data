4b1d62b32eee8d36f354cb188eed0b43d64b6360
LORDyyyyy <mlord6271@gmail.com>
2024-02-22 02:10:56 +0200

    task 1 style

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,6 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
@@ -27,11 +26,10 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ adds a new user """
+        """adds a new user"""
         user = User(email=email, hashed_password=hashed_password)
 
         self._session.add(user)
         self._session.commit()
 
         return user
-
